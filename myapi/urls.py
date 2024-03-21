a71943af06903cf869612359e13cddcc1c6f4428
Satyam Sharma <satyam@instaastro.in>
2024-03-21 10:38:27 +0530

    Added Manga Image API

--- myapi/urls.py
+++ myapi/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('carousel/', views.carousel, name='carousel'),
     path('mangalist/', views.MangaViewSet.as_view({'get': 'list'}), name='mangalist'),
     path('mangachapter/', views.manga_chapter_list, name='mangachapter'),
+    path('chapter_images/', views.manga_chapters_images, name='mangachapters_images'),
 ]
--- myapi/views.py
+++ myapi/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
 
 from myapi.models import CarouselImage, MangaChapters, MangaList
 from myapi.serializers import CarouselImageSerializer, MangaChapterSerializer, MangaListSerializer
+from zipfile import ZipFile
+from PIL import Image
+import io
+import base64
 
 
 @api_view(['GET'])
@@ -29,3 +33,27 @@ def manga_chapter_list(request):
     chapters = MangaChapters.objects.filter(manga__pk=manga_pk)
     data = MangaChapterSerializer(chapters, many=True).data
     return Response({'chapters': data})
+
+
+@api_view(['GET'])
+def manga_chapters_images(request):
+    chapter_pk = request.GET.get('pk')
+    try:
+        chapter = MangaChapters.objects.get(pk=chapter_pk)
+    except MangaChapters.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    file_path = chapter.manga_file.path
+
+    with ZipFile(file_path, 'r') as zip_ref:
+        image_names = zip_ref.namelist()
+        images = []
+        for image_name in image_names:
+            with zip_ref.open(image_name) as file:
+                img = Image.open(file)
+                img_byte_arr = io.BytesIO()
+                img.save(img_byte_arr, format='PNG')
+                img_byte_arr = img_byte_arr.getvalue()
+                images.append(base64.encodebytes(img_byte_arr).decode('ascii'))
+
+    return Response({'images': images})
