71adbd9a360d0ebc8239266ab47d4eb35251af5c
Satyam Sharma <satyamsharma3524@gmail.com>
2025-04-15 21:00:05 +0530

    Changes for Production env

--- bin/build-and-run.sh
+++ bin/build-and-run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# Get the current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_DIR=$DIR/..
+PROJECT_DIR="$DIR/.."
 
-cd $PROJECT_DIR
+# Load .env file
+export $(grep -v '^#' "$PROJECT_DIR/.env" | xargs)
 
-docker-compose -f docker-compose-prod.yml build --pull
-docker-compose -f docker-compose-prod.yml up -d --remove-orphans
+cd "$PROJECT_DIR"
+
+if [ "$ENV" == "Prod" ]; then
+  COMPOSE_FILE="docker-compose-prod.yml"
+else
+  COMPOSE_FILE="docker-compose.yml"
+fi
+
+docker-compose -f $COMPOSE_FILE build --pull
+docker-compose -f $COMPOSE_FILE up -d --remove-orphans
