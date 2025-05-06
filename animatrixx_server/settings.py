6d1ad910db367f8a09f06fe91acc9f5c1a32b7ea
Satyam Sharma <satyamsharma3524@gmail.com>
2025-05-06 12:58:45 +0530

    Added Pagination for the Manga List api

--- animatrixx_server/settings.py
+++ animatrixx_server/settings.py
@@ -113,7 +113,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 12,
 }
 
 REST_AUTH_REGISTER_SERIALIZERS = {
