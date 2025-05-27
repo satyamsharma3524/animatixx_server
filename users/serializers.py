aaf137946003a10c534ad2cd1fc3cc87b22e601a
Satyam Sharma <satyamsharma3524@gmail.com>
2025-05-27 22:16:30 +0530

    Added Firstname and Lastname for User Registration

--- users/serializers.py
+++ users/serializers.py
@@ -15,10 +15,13 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'first_name', 'last_name',
+                  'username', 'email', 'password')
 
     def create(self, validated_data):
         user = User.objects.create_user(
+            first_name=validated_data.get('first_name', 'Anonymous'),
+            last_name=validated_data.get('last_name', 'User'),
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
